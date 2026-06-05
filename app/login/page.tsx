e0d16948dd67cff2e82d245aa23e829ac9bef9c0
Ben Everly <benjamin.g.everly@gmail.com>
2026-06-05 14:21:34 -0500

    docs(auth): add a launch TODO for the login privacy disclosure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -35,6 +35,7 @@ export default async function LoginPage({
             {notice.message}
           </p>
         )}
+        {/* TODO(launch): data disclosure + privacy policy link go here once a policy exists. */}
         <GoogleSignInButton next={next} />
       </div>
     </main>
